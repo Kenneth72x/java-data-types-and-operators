5418bb6ef08253da3abd14369598e742582a501d
Kenneth Rothstein <skottrod@me.com>
2021-08-30 04:48:14 -0700

    Initial commit

---
+++ BoolDemo.java
@@ -0,0 +1,25 @@
+/*
+The boolean type represents true/false values. Java defines the values true and false using the reserved words true and false. 
+
+Demonstrate boolean values.
+*/
+
+class BoolDemo {
+    public static void main(String args[]){
+        boolean b;
+
+        b = false;
+        System.out.println("b is " + b);
+        b = true;
+        System.out.println("b is " + b);
+
+        //A boolean value can control the if statement
+        if(b) System.out.println("This is executed.");
+
+        b = false;
+        if(b) System.out.println("This is not executed.");
+
+        //outcome of a relational operator is a boolean value
+        System.out.println("10 > 9 is " + (10 > 9));
+    }
+}
