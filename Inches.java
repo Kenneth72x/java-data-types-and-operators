5a11c28957ab0187021ded6c85d7576471409257
Kenneth Rothstein <skottrod@me.com>
2021-08-29 17:04:10 -0700

    CFinal code computing the number of cubic inches in 1 cubic mile.

--- Inches.java
+++ Inches.java
@@ -3,5 +3,13 @@ Compute the number cubic inches in 1 cubic mile.
 */
 
 class Inches {
-    
+    public static void main(String args[]){
+        long ci;
+        long im;
+
+        im = 5280 *12;
+        ci = im * im * im;
+
+        System.out.println("There are " + ci + " cubic inches in cubic mile.");
+    }
 }
