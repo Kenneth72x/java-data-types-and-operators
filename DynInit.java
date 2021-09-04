f1e9d065ba56e70d3a8ab4675ba61d5175fe99a9
Kenneth Rothstein <skottrod@me.com>
2021-09-04 16:07:29 -0700

    Added Class DynInit

--- DynInit.java
+++ DynInit.java
@@ -2,5 +2,12 @@
 
 
 class DynInit {
-    
+    public static void main(String args[]){
+        double radius = 4, height = 5,;
+        
+        //dynamically initialize volume
+        double volume = 3.1416 * radius * radius * height; //<---volume is dynamically initialized at run time. 
+
+        
+    }
 }
