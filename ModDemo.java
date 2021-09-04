af11f5680258c3874378f2962afb144456b1881a
Kenneth Rothstein <skottrod@me.com>
2021-09-04 16:48:34 -0700

    Class created and completed

---
+++ ModDemo.java
@@ -0,0 +1,19 @@
+//Demonstrate the % operator.
+
+
+class ModDemo {
+    public static void main(String arg[]){
+        int iresult, irem;
+        double dresult, drem;
+
+        iresult = 10 / 3;
+        irem = 10 % 3;
+
+        dresult = 10.0 / 3.0;
+        drem = 10.0 % 3.0;
+
+        System.out.println("Result and remainder of 10 / 3: " + iresult + " " + irem);
+        System.out.println("Result and remainder of 10.0 / 3.0: " + dresult + " " + drem);
+    }
+}
+//As you can see, the % operator yields a remainder of 1 for both integer and floating-point operations. 
