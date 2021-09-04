5cce085c1e97919c1bb98b6dd74b1a0f6ec4c768
Kenneth Rothstein <skottrod@me.com>
2021-09-04 16:09:41 -0700

    completed class

--- DynInit.java
+++ DynInit.java
@@ -3,11 +3,11 @@
 
 class DynInit {
     public static void main(String args[]){
-        double radius = 4, height = 5,;
+        double radius = 4, height = 5;
         
         //dynamically initialize volume
         double volume = 3.1416 * radius * radius * height; //<---volume is dynamically initialized at run time. 
 
-        
+        System.out.println("Volume is " + volume);
     }
 }
